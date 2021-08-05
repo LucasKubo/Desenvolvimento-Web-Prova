7095db7c0bdc0406b674385e387ecb3469cd0a62
LucasKubo <lucaskbk2@gmail.com>
2021-08-04 23:07:35 -0300

    Reorganizando código - parquímetro

--- js/parquimetro.js
+++ js/parquimetro.js
@@ -3,69 +3,77 @@ function salvarPlaca(event){
     let placaIn = document.querySelector("#placaIn");
     let placa = placaIn.value;
 
-    if(placa == ""){ //Verifica se uma placa foi inserida
-        return false; //Indica que a placa não foi salva
+    if(placa){ //Verifica se uma placa foi inserida
+        localStorage.setItem("placa",placa);
+        return true; //indicia que a placa foi salva
     }
-
-    localStorage.setItem("placa",placa);
-    let placaSalva = localStorage.getItem("placa");
-    return true; //Indica que a placa foi salva
+    return false; //Indica que a placa não foi salva
 }
 
-function calculaTicket(event) {
+//Recebe um valor e calcula o tempo reservado
+function calculaTempo(valorPago) { 
+    let tempo = 0;
+    
+    if(valorPago < 1){
+        return tempo;
+    }else if(valorPago < 1.75){
+        return tempo = 30;
+    }else if(valorPago < 3){
+        return tempo = 60;
+    }else{
+        return tempo = 120;
+    }
+}
+
+//Recebe um valor e calcula o troco
+function calculaTroco(valorPago){
+    let troco = 0;
+    
+    if(valorPago < 1.75){
+        return troco = valorPago - 1;
+    }else if(valorPago < 3){
+        return troco = valorPago - 1.75;
+    }else{
+        return troco = valorPago - 3;
+    }
+}
+
+function mostraTicket(event){
     event.preventDefault();
     let valorIn = document.querySelector("#valorIn");
-    let tempoOut = document.querySelector(".tempoOut");
-    let trocoOut = document.querySelector(".trocoOut");
-    let placaOut = document.querySelector(".placaOut")
+    let valorPago = valorIn.value;
+    let ticketResultado = document.querySelector(".ticket-resultado");
 
-    let valorPago = Number(valorIn.value);
-    let troco = 0;
-    let tempo = 0;
+    //Limpando HTML e localStorage
+    ticketResultado.textContent = "";
+    localStorage.clear();
 
-    tempoOut.textContent = "";
-    trocoOut.textContent = "";
-    placaOut.textContent = "";
-
-    if(!salvarPlaca(event)){ //Verifica se a placa foi salva
-        placaOut.textContent = "Digite uma placa !";
+    //Verifica se a placa foi salva
+    if(!salvarPlaca(event)){ 
+        ticketResultado.textContent = "Digite uma placa !";
         placaIn.focus();
         return;
     }
 
-    if(isNaN(valorPago)){
-        tempoOut.textContent = "Valor inválido";
+    //calcula o tempo
+    let tempo = calculaTempo(valorPago);
+
+    if(tempo == 0){ //Se não tiver tempo reservado, significa que o valor é insuficiente
+        ticketResultado.textContent = "Valor insuficiente";
         valorIn.focus();
         return;
-    }
-    if(valorPago < 1){
-        tempoOut.textContent = "Valor insuficiente";
-        valorIn.focus();
-        return;
-    }else if(valorPago < 1.75){
-        tempo = 30;
-        troco = valorPago - 1;
-        tempoOut.textContent = "Você tem " + tempo + " minutos de estacionamento";
-        if (troco != 0){
-            trocoOut.textContent = "Seu troco é R$ " + troco.toFixed(2);
-        }
-    }else if(valorPago < 3){
-        tempo = 60;
-        troco = valorPago - 1.75;
-        tempoOut.textContent = "Você tem " + tempo + " minutos de estacionamento";
-        if (troco != 0){
-            trocoOut.textContent = "Seu troco é R$ " + troco.toFixed(2);
-        }
     }else{
-        tempo = 120;
-        troco = valorPago - 3;
-        tempoOut.textContent = "Você tem " + tempo + " minutos de estacionamento";
-        if (troco != 0){
-            trocoOut.textContent = "Seu troco é R$ " + troco.toFixed(2);
+        //Após validar o tempo, verifica se há troco
+        let troco = calculaTroco(valorPago);
+        if (troco == 0){
+            ticketResultado.textContent = "Você tem " + tempo + " minutos de estacionamento";
+        }else{
+            ticketResultado.innerHTML = "Você tem " + tempo + " minutos de estacionamento" + "<br>"+
+                                            "Seu troco é R$ " + troco.toFixed(2);
         }
-    }
-    localStorage.setItem("tempo",tempo);
+        localStorage.setItem("tempo",tempo);
+    }  
 }
 
 let botaoEnviar = document.getElementById("botao-enviar");
-botaoEnviar.addEventListener("click",calculaTicket);
+botaoEnviar.addEventListener("click",mostraTicket);
